9c4e05c66d6dd5b0ca19d32a63822355a6a368c7
unknown <brunoelibio11@gmail.com>
2025-08-12 17:13:07 -0300

    Primeiro commit, spring initlzr

---
+++ src/main/java/com/bruno/aprovado/AprovadoApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.aprovado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprovadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprovadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/aprovado/AprovadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.aprovado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprovadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
